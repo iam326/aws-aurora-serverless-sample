1e09bbc6b7ae59bd4f4a9ec0dd412a62998c7821
t.takahashi <takahashi.developer@gmail.com>
2019-12-29 23:07:01 +0900

    strict database access

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ set -euo pipefail
 readonly STACK_NAME="aws-aurora-serverless-sample"
 readonly BUCKET_NAME="iam326.${STACK_NAME}"
 
+readonly NAME_PREFIX="aws-aurora-sl-sample"
+readonly DATABASE_CLUSTER_NAME="${NAME_PREFIX}-database-cluster"
+
 # execute only the first time
 # aws s3 mb "s3://${BUCKET_NAME}"
 
@@ -17,12 +20,14 @@ sam package \
 sam deploy \
   --template-file packaged.yaml \
   --stack-name ${STACK_NAME} \
-  --capabilities CAPABILITY_IAM
+  --capabilities CAPABILITY_IAM \
+  --parameter-overrides \
+    NamePrefix=${NAME_PREFIX} \
+    DatabaseClusterName=${DATABASE_CLUSTER_NAME}
 
 rm packaged.yaml
 
-DB_CLUSTER_IDENTIFIER="aws-aurora-sl-sample-database-cluster"
 aws rds modify-db-cluster \
-  --db-cluster-identifier ${DB_CLUSTER_IDENTIFIER} \
+  --db-cluster-identifier ${DATABASE_CLUSTER_NAME} \
   --enable-http-endpoint \
   --apply-immediately
